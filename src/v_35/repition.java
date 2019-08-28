7b4c22a12b7aa957da9d03b803450b39114f1354
Thekolllol90 <neosvarre@hotmail.se>
2019-08-28 11:54:42 +0200

    hello

---
+++ src/v_35/repition.java
@@ -0,0 +1,34 @@
+package v_35;
+
+import_java_util scanner;
+
+public class repition {
+	public static void main(String[] args) {
+		
+	summera_array();
+	
+	vander_ord();
+	
+	}
+	
+	public static void summera_array() {
+		int[] array = {1, 4, 8};
+		int x = 0;
+		
+		for(int i = 0; i < array.length; i++) {
+			x = x + array[i];
+		}
+		
+		System.out.print(x);
+	}
+	
+	public static void vander_ord() {
+		
+		String one = ;
+		String two = "";
+		String three = "";
+		
+		System.out.print(three + " " + two + " " + one);
+		
+	}
+}
